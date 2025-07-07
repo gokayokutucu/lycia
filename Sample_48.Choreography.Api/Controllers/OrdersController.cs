16c22cafdb7b69b48b69c1851c08f1d121794777
gokcekuvandik <70231387+gokcekuvandik@users.noreply.github.com>
2025-07-07 17:41:38 +0300

    pop the stahed changes

---
+++ Sample_48.Choreography.Api/Controllers/OrdersController.cs
@@ -0,0 +1,21 @@
+﻿using Lycia.Saga.Abstractions;
+using Sample.Shared.Messages.Commands;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Sample_48.Choreography.Api.Controllers
+{
+    public class OrdersController : ApiController
+    {
+        private readonly IEventBus _eventBus;
+        public OrdersController(IEventBus eventBus) => _eventBus = eventBus;
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Post([FromBody] CreateOrderCommand command)
+        {
+            await _eventBus.Send(command);
+            return StatusCode(HttpStatusCode.Accepted);
+        }
+    }
+}
---
+++ Sample_48.Choreography.Api/Global.asax.cs
@@ -0,0 +1,41 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using Lycia.Extensions.Eventing;
+using Lycia.Saga.Abstractions;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Sample_48.Choreography.Api
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            // Usual Web API config
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            var builder = new ContainerBuilder();
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<RabbitMqEventBus>().As<IEventBus>().InstancePerRequest();
+
+            // If you have other services, register them here
+            // builder.RegisterType<SomeService>().As<ISomeService>();
+
+            var container = builder.Build();
+
+            var resolver = new AutofacWebApiDependencyResolver(container);
+            GlobalConfiguration.Configuration.DependencyResolver = resolver;
+
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
