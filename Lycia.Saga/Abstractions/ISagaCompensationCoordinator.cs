0d65c89387ba498200980ddd1b7ba354a7cc8e3e
Gokay Okutucu <okutucugokay@gmail.com>
2025-07-07 13:15:22 +0300

    Move source code under src folder

--- Lycia.Saga/Abstractions/ISagaCompensationCoordinator.cs
+++
@@ -1,9 +0,0 @@
-using Lycia.Messaging;
-
-namespace Lycia.Saga.Abstractions;
-
-public interface ISagaCompensationCoordinator
-{
-    Task CompensateAsync(Guid sagaId, Type failedStepType);
-    Task CompensateParentAsync(Guid sagaId, Type stepType, IMessage message);
-}
