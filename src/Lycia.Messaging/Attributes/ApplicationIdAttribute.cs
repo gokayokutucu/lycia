ffab8e2a94eb6c9d8dd40489d3f5b8772b758c86
Gokay Okutucu <okutucugokay@gmail.com>
2025-07-09 12:33:00 +0300

    Remove applicationId attr

--- src/Lycia.Messaging/Attributes/ApplicationIdAttribute.cs
+++
@@ -1,7 +0,0 @@
-namespace Lycia.Messaging.Attributes;
-
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
-public class ApplicationIdAttribute(string applicationId) : Attribute
-{
-    public string ApplicationId { get; } = applicationId;
-}
