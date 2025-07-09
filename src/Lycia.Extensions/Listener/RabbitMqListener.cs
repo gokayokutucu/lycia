fdd7a6925e4b4198a3a5f3006e991b3514ae24e1
Gokay Okutucu <okutucugokay@gmail.com>
2025-07-09 12:58:31 +0300

    Move listener

--- src/Lycia.Extensions/Listener/RabbitMqListener.cs
+++ src/Lycia.Extensions/Listener/RabbitMqListener.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace Lycia.Infrastructure.Listener;
+namespace Lycia.Extensions.Listener;
 
 public class RabbitMqListener(
     IServiceProvider serviceProvider,
