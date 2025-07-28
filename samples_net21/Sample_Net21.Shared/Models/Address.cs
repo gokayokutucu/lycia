8d5c9cdd661e31672937f628e623658c39654c7e
gokcekuvandik <70231387+gokcekuvandik@users.noreply.github.com>
2025-07-28 17:41:30 +0300

    after merge refactors

--- samples_net21/Sample_Net21.Shared/Models/Address.cs
+++ samples_net21/Sample_Net21.Shared/Models/Address.cs
@@ -7,13 +7,13 @@ namespace Sample_Net21.Shared.Models
     public class Address : BaseModel
     {
         public string Title { get; set; } = string.Empty;
-        public int CountryId { get; set; }
-        public int RegionId { get; set; }
-        public int CityId { get; set; }
-        public int StreetId { get; set; }
-        public int BuildingId { get; set; }
-        public int Floor { get; set; }
-        public int Apartment { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Building { get; set; }
+        public string Floor { get; set; }
+        public string Apartment { get; set; }
         public string ZipCode { get; set; } = string.Empty;
     }
 }
