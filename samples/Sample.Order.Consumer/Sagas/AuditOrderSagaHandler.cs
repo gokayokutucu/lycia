7ea39f7e8b21a19a43ea9dc82c6178bdde2454cd
Gokay Okutucu <okutucugokay@gmail.com>
2025-07-30 14:26:56 +0300

    Fix circular chain duplicated key error

--- samples/Sample.Order.Consumer/Sagas/AuditOrderSagaHandler.cs
+++ samples/Sample.Order.Consumer/Sagas/AuditOrderSagaHandler.cs
@@ -9,12 +9,25 @@ public class AuditOrderSagaHandler : ReactiveSagaHandler<OrderCreatedEvent>
     {
         try
         {
-            await Context.MarkAsComplete<OrderCreatedEvent>();
+            await Context.MarkAsFailed<OrderCreatedEvent>();
         }
         catch (Exception e)
         {
             Console.WriteLine($"🚨 Audit failed: {e.Message}");
             await Context.MarkAsFailed<OrderCreatedEvent>();
+        }
+    }
+
+    public override async Task CompensateAsync(OrderCreatedEvent message)
+    {
+        try
+        {
+            await Context.MarkAsCompensated<OrderCreatedEvent>();
+        }
+        catch (Exception e)
+        {
+            await Context.MarkAsCompensationFailed<OrderCreatedEvent>();
+            throw;
         }
     }
 }
