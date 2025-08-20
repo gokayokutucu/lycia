2093c17c2f384066b13a38a8c18f8deed5375c64
Gokay Okutucu <okutucugokay@gmail.com>
2025-08-20 13:15:00 +0300

    Remove StepMessages dictionary for cleaning

--- src/Lycia.Saga/Handlers/StartCoordinatedSagaHandler.cs
+++ src/Lycia.Saga/Handlers/StartCoordinatedSagaHandler.cs
@@ -27,7 +27,6 @@ public abstract class StartCoordinatedSagaHandler<TMessage, TSagaData> :
 
     protected async Task HandleAsyncInternal(TMessage message, CancellationToken cancellationToken = default)
     {
-        Context.RegisterStepMessage(message); // Mapping the message to the saga context
         try
         {
             cancellationToken.ThrowIfCancellationRequested();
@@ -45,7 +44,6 @@ public abstract class StartCoordinatedSagaHandler<TMessage, TSagaData> :
 
     protected async Task CompensateAsyncInternal(TMessage message, CancellationToken cancellationToken = default)
     {
-        Context.RegisterStepMessage(message); // Mapping the message to the saga context
         try
         {
             cancellationToken.ThrowIfCancellationRequested();
