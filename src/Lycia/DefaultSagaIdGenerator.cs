a4f504e3fda016d943bb78f8a6a8be0d38c5935f
Gokay Okutucu <okutucugokay@gmail.com>
2025-11-24 15:10:15 +0200

    Add summary comments

--- src/Lycia/DefaultSagaIdGenerator.cs
+++ src/Lycia/DefaultSagaIdGenerator.cs
@@ -7,7 +7,15 @@ using Lycia.Saga.Utility;
 
 namespace Lycia;
 
+/// <summary>
+/// Represents the default implementation of the <see cref="ISagaIdGenerator"/> interface,
+/// responsible for generating unique identifiers for sagas using a version 7 GUID.
+/// </summary>
 public class DefaultSagaIdGenerator : ISagaIdGenerator
 {
+    /// <summary>
+    /// Generates a new unique identifier for a Saga using the Version 7 UUID format.
+    /// </summary>
+    /// <returns>A globally unique identifier (GUID) in Version 7 UUID format.</returns>
     public Guid Generate() => GuidV7.NewGuidV7();
 }
